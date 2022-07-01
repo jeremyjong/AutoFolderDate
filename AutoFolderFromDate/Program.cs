ea1a78ee016c581dacd0ea48c538fe0e0ce85dcf
Jeremy <jeremyjong@gmail.com>
2022-07-01 22:31:53 +0900

    first commit
    Signed-off-by: Jeremy <jeremyjong@gmail.com>

---
+++ AutoFolderFromDate/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoFolderFromDate
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+
+
+
+            DateTime today = DateTime.Today;
+            
+            var files = Directory.GetFiles("L:\\(Manga)\\");
+
+            
+
+
+            foreach (var f in files)
+            {
+
+                FileInfo fInfo = new FileInfo(f);
+
+                var fLastTime = fInfo.LastWriteTime.ToString("yyyyMMdd");
+                string destinationDirectory = "L:\\(Manga)\\" + fLastTime;
+
+                System.IO.Directory.CreateDirectory(destinationDirectory);
+                System.IO.File.Move(f, destinationDirectory + "\\" + Path.GetFileName(f));
+                
+            }
+
+
+        }
+    }
+}
